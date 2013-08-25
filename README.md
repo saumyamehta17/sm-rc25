23d6c9d56cf8035208e708ae8f723e2ae996a869
sweety <smehta1289@gmail.com>
2013-08-25 23:54:42 +0530

    sql injection removing

---
+++ README.md
@@ -0,0 +1,28 @@
+railscast sm-rc25
+=================
+
+SqlInjection...
+Placing user input directly into sql query..
+So not allow user to into sql and make sql syntax error include escaping by using '?'..
+
+Cloning
+```
+Git clone ""
+```
+scaffolding...
+```
+#todo
+```
+create seraching feature on index.html.erb
+```
+use form_tag..
+```
+implement search in controller index 
+```
+check without using '?' vl give error of sql syntax
+check with '?'
+```
+run server
+```
+rails s
+```
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmRc25::Application.config.secret_token = 'a2d026c70b54a2b0fa35f2aabf0b9f5de8265a4b9b7eba432274017fab70958a86637cbd87126da21a4d37a7ea0b552439ca3d9a5f478f4cf97eb3f81a3fb33e'
